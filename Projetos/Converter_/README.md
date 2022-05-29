e900fe3e39b907471ffad4e25174462276300c8e
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-05-28 22:09:52 -0300

    Documentação simples do primeiro projeto

--- Projetos/Converter_/README.md
+++ Projetos/Converter_/README.md
@@ -1,2 +1,24 @@
-##  :construction: Em construção :construction:
+# Converter
 ---
+
+Criando um conversor simples com PySimpleGUI, utilizando elementos básicos da biblioteca e mostrando output
+dos resultado.
+
+## Exemplos da aplicação
+    
+#### km to mile
+
+![km_to_mile](https://media.discordapp.net/attachments/887544607599120404/980275209712042044/Capturar.PNG)
+---
+
+
+#### kg to pounds
+
+![kg_to_pounds](https://media.discordapp.net/attachments/887544607599120404/980275209070333962/Capturar2.PNG)
+---
+
+#### seconds to minutes
+
+![sec_to_min](https://media.discordapp.net/attachments/887544607599120404/980275209414275152/Capturar3.PNG)
+---
+
--- Projetos/Converter_/main.py
+++ Projetos/Converter_/main.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg 
 
+# Tipos de elementos utilizados na tela do GUI, criando o layout
 layout = [
     [sg.Input(key='-INPUT-'), sg.Spin(
         ['km to mile', 
@@ -10,32 +11,43 @@ layout = [
     
 ]
 
+# Criando tela do PySimpleGUI
 window = sg.Window('Converter', layout)
 
+# Loop Principal
 while True:
     event, values = window.read()
 
+    # Evento para caso clique no 'X', o aplicativo feche e pare o loop
     if event == sg.WIN_CLOSED:
         break
 
+    # Pegando valor do input caso clique no botão com key '-CONVERT-'
     if event == '-CONVERT-':    
         input_value = values['-INPUT-']
+
+        # Verificando se o valor é numérico
         if input_value.isnumeric():
+            
+            # Match case para calcular de acordo com a opção selecionada no 'spin'
             match values['-UNITS-']:
                 case 'km to mile':
                     output = round(float(input_value) * 0.06214, 2)
                     output_string = f'{input_value} km are {output} miles.'
+
                 case 'kg to pound':
                     output = round(float(input_value) * 2.20462, 2)
                     output_string = f'{input_value} kg are {output} pounds.'
+
                 case 'sec to min':
                     output = round(float(input_value) / 60, 2)
                     output_string = f'{input_value} seconds are {output} minutes.'
             
+        # Imprimindo resultado do calculo de acordo com a oçpão escolhida
             window['-OUTPUT-'].update(output_string)
         else:
             window['-OUTPUT-'].update('Please enter a number')
 
-
+# Fechando janela criada da aplicação
 window.close()
 
